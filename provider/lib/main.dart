1ac26e05f3f55fe146822388adfa276aa0bec138
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-10-31 13:40:07 +0500

    simple stateful widget counter example

--- provider/lib/main.dart
+++ provider/lib/main.dart
@@ -9,55 +9,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home_View(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class Home_View extends StatefulWidget {
+  const Home_View({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home_View> createState() => _Home_ViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+int counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _Home_ViewState extends State<Home_View> {
   @override
   Widget build(BuildContext context) {
+    print(build);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              counter.toString(),
+              style: TextStyle(fontSize: 30),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          counter++;
+          print(counter);
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
